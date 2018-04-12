afab7920712dc02266964c0cd474aaadf5ca010f
Ferdinand Holzer <ferdinand.holzer@gmail.com>
2018-04-12 08:45:08 +0200

    Use npm package for reading lines

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const elasticsearch = require('elasticsearch'),
-    uuidv4 = require('uuid/v4');
+    uuidv4 = require('uuid/v4'),
+    throughLineReader = require('through2-linereader'),
+    process = require('process');
 
 var client = new elasticsearch.Client({
     host: "nuc:9200"
@@ -11,7 +13,8 @@ const idRoomMapping = {
     "43937": "Wintergarten",
     "43938": "Küche",
     "43939": "Vorzimmer",
-    "43940": "Wohnzimmer"
+    "43940": "Wohnzimmer",
+    "43941": "Schlafzimmer"
 };
 
 const UHOME_FIELD_MAPPING = {
@@ -25,91 +28,50 @@ const UHOME_FIELD_NAMES = [
     "voltage"
 ]
 
+var env = Object.create(process.env);
+env.TZ = 'UTC';
+
 var spawn = require('child_process').spawn,
-    ls = spawn('rtl_433', ['-f', '433950000', '-F', 'json', '-g', '35']);
+    ls = spawn('rtl_433', ['-f', '433950000', '-F', 'json', '-R', '99', '-g', '0'], { env: env });
 
-var buffer = '';
 
-/**
- *  * Parses a string containing one or multiple JSON encoded objects in the string.
- *   * The result is always an array of objects.
- *    *
- *     * @param  {String} data
- *      * @return {Array}
- *       */
-function parseJson(data) {
-    data = data.replace('\n', '', 'g');
-
-    var
-        start = data.indexOf('{'),
-        open = 0,
-        i = start,
-        len = data.length,
-        result = [];
-
-    for (; i < len; i++) {
-        if (data[i] == '{') {
-            open++;
-        } else if (data[i] == '}') {
-            open--;
-            if (open === 0) {
-                result.push(JSON.parse(data.substring(start, i + 1)));
-                start = i + 1;
-            }
-        }
-    }
-
-    return result;
-}
-
-ls.stdout.on('data', function(data) {
-    buffer += data;
-    console.log(buffer);
+ls.stdout.pipe(throughLineReader()).on('data', function(data) {
 
     try {
-        var dataSet = parseJson(buffer);
-        console.log(dataSet);
-
-        if (dataSet.length === 0)
-            return;
+        var data = JSON.parse(data);
 
         var now = new Date(),
             indexName = "sensors-" + now.getUTCFullYear() + "." + (now.getUTCMonth() + 1) + "." + now.getUTCDate();
 
-        for (var i = 0; i < dataSet.length; i++) {
-            var data = dataSet[i];
-            switch (data.model) {
-                case 'µHome':
-                    var doc = {
-                        "model": "uhome",
-                        "@timestamp": data.time
-                    };
+        switch (data.model) {
+            case 'µHome':
+                var doc = {
+                    "model": "uhome",
+                    "@timestamp": data.time
+                };
 
-                    if(data.hasOwnProperty("id") && idRoomMapping.hasOwnProperty(data.id)) {
-                        doc.room = idRoomMapping[data.id];
+                if(data.hasOwnProperty("id") && idRoomMapping.hasOwnProperty(data.id)) {
+                    doc.room = idRoomMapping[data.id];
+                }
+
+                UHOME_FIELD_NAMES.forEach(f => {
+                    if(data.hasOwnProperty(f) && data[f] > 0) {
+                        let pname = UHOME_FIELD_MAPPING.hasOwnProperty(f) ? UHOME_FIELD_MAPPING[f] : f;
+                        doc[pname] = data[f];
                     }
-
-                    UHOME_FIELD_NAMES.forEach(f => {
-                        if(data.hasOwnProperty(f) && data[f] > 0) {
-                            let pname = UHOME_FIELD_MAPPING.hasOwnProperty(f) ? UHOME_FIELD_MAPPING[f] : f;
-                            doc[pname] = data[f];
-                        }
-                    });
-                    client.create({
-                        index: indexName,
-                        type: "doc",
-                        id: uuidv4(),
-                        body: doc
-                    }, (err, res) => {
-                        if(err) {
-                            console.log("ERROR", err);
-                        }
-                    });
-                    break;
-            }
+                });
+                client.create({
+                    index: indexName,
+                    type: "doc",
+                    id: uuidv4(),
+                    body: doc
+                }, (err, res) => {
+                    if(err) {
+                        console.log("ERROR", err);
+                    }
+                });
+                break;
         }
-
-        buffer = '';
     } catch (e) {
         console.log(e);
     }
@@ -121,4 +83,5 @@ ls.stderr.on('data', function(data) {
 
 ls.on('exit', function(code) {
     console.log('child process exited with code ' + code.toString());
+    process.exit(-1);
 });
